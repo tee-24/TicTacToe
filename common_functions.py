b4519a9be5cf339a6656881acd0412682e26ac79
Towbie Idowu <towbieidowu@gmail.com>
2024-03-08 22:06:24 +0000

    Install Black library

--- common_functions.py
+++ common_functions.py
@@ -1,5 +1,7 @@
 # Global variables
-board = {1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9'}
+board = {
+    1: "1", 2: "2", 3: "3", 4: "4", 5: "5", 6: "6", 7: "7", 8: "8", 9: "9"
+}
 
 
 def get_num_of_players():
@@ -11,28 +13,30 @@ def get_num_of_players():
 
     while answer not in [1, 2]:
         try:
-            answer = int(input('1 or 2 players? '))
+            answer = int(input("1 or 2 players? "))
         except ValueError:
-            print('\nInvalid choice, please choose 1 or 2')
-            
+            print("\nInvalid choice, please choose 1 or 2")
+
             continue
 
         if answer not in [1, 2]:
-            print('\nInvalid choice, please choose 1 or 2')
-    
+            print("\nInvalid choice, please choose 1 or 2")
+
     return answer
+
 
 def display_board(board):
     """
     Print out the game board
     """
-    print(f' {board[1]} | {board[2]} | {board[3]}')
-    print(f' {board[4]} | {board[5]} | {board[6]}')
-    print(f' {board[7]} | {board[8]} | {board[9]}')
+    print(f" {board[1]} | {board[2]} | {board[3]}")
+    print(f" {board[4]} | {board[5]} | {board[6]}")
+    print(f" {board[7]} | {board[8]} | {board[9]}")
+
 
 def check_tie(board):
     """
-    Will return True if the board is full 
+    Will return True if the board is full
     or will return False if the board is not full
     """
 
@@ -41,9 +45,10 @@ def check_tie(board):
             return False
     return True
 
+
 def reset_board(board):
     """
     Resets the board to default
     """
-    for spot in range(1,10):
+    for spot in range(1, 10):
         board[spot] = str(spot)
