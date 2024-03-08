fd29fe1b4f170ff943edc332e6b86849ed18ef2b
Towbie Idowu <towbieidowu@gmail.com>
2024-03-08 12:01:46 +0000

    Add code to get player 2 name

--- two_player.py
+++ two_player.py
@@ -20,3 +20,24 @@ ef get_player1_name():
     print(f'{player1} is Player 1')
 
     return player1
+
+def get_player2_name():
+    """
+    Get the name of Player 2
+    """
+    player2 = ''
+
+    while len(player2) < 3 or not player2.isalpha() or len(player2) > 10:
+        player2 = input('\nPlayer 2, enter your name: ').capitalize()
+
+        if player2.isalpha() == False:
+            print('Name can only contain letters, please try again')
+        elif len(player2) < 3:
+            print('Name must be a minimum of 3 letters, please try again')
+        elif len(player2) > 10:
+            print('Name can only be a maximum of 10 letters, please try again')
+        
+    print(f'{player2} is Player 2')
+
+    return player2
+
