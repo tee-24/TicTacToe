ed15e68615d429c72594059988430e3abc80579d
Towbie Idowu <towbieidowu@gmail.com>
2024-03-08 11:39:28 +0000

    Add intro and instructions

--- run.py
+++ run.py
@@ -1,2 +1,9 @@
 def run_game():
-    pass
+    # Intro
+    print('\nHello! Welcome to Tic Tac Toe :grinning_face_with_big_eyes:')
+    # Instructions
+    print('Instructions:')
+    print('First to get 3 in a row wins!')
+    # Empty line
+    print()
+   
