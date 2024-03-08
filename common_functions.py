3667598e7fe346bffb27b10b4ff7910df298fb16
Towbie Idowu <towbieidowu@gmail.com>
2024-03-08 11:34:44 +0000

    Add code to display board

--- common_functions.py
+++ common_functions.py
@@ -1,3 +1,7 @@
+# Global variables
+Board = {1:'1', 2:'2', 3:'3', 4:'4', 5:'5', 6:'6', 7:'7', 8:'8', 9:'9'}
+
+
 def get_num_of_players():
     """
     Get the number of players from user
@@ -17,3 +21,11 @@ def get_num_of_players():
             print('\nInvalid choice, please choose 1 or 2')
     
     return answer
+
+def display_board(Board):
+    """
+    Print out the game board
+    """
+    print(f' {Board[1]} | {Board[2]} | {Board[3]}')
+    print(f' {Board[4]} | {Board[5]} | {Board[6]}')
+    print(f' {Board[7]} | {Board[8]} | {Board[9]}')
